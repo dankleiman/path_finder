29556f6b53ac44c3f9f8e3aea9835d7c70d981c3
Dan Kleiman <info@dankleiman.com>
2014-05-23 14:43:02 -0400

    Returns true or false for every possible array path

--- new_simple.rb
+++ new_simple.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 def path_finder(value, structure, path = [])
+  if value == structure
+    path << true
+    return path
+  end
+
   if structure.include?(value)
     if structure.respond_to?(:each)
       structure.each do |element|
         if element == value
-          path << "[#{structure.index(element)}]"
+          # path << "[#{structure.index(element)}]"
           path << true
+          # return true
         else
-          path << false
           path_finder(value, element, path)
         end
       end
     else
       if structure == value
-        path << "[#{structure.index(element)}]"
+        # path << "[#{structure.index(element)}]"
         path << true
       else
         # path << "We got here on 20"
@@ -26,7 +31,7 @@ def path_finder(value, structure, path = [])
         structure.each do |element|
           path = path_finder(value, element, path)
           if path.last == true
-            path.insert(0, "[#{structure.index(element)}]")
+            # path.insert(0, "[#{structure.index(element)}]")
           end
         end
     else
@@ -41,13 +46,20 @@ def path_cleanup(raw_path)
   raw_path.reject!{|i| i == false || i == true}
   clean_path = raw_path.join
 end
+
+
+
+
+
 favorite_movies = [[['title','The Big Lebowski'], ['year_released', '1998'], ['director', 'Joel Coen']],
   [['title', 'The Shining'],['year_released', '1980'], ['director', 'Stanley Kubrick']],
   [['title', 'Troll 2'],['year_released', '1990'], ['director', 'Claudio Fragasso']]]
 
-# favorite_movies = [['title','The Big Lebowski'], ['year_released', '1998'], ['director', 'Joel Coen']]
+# favorite_movies = [['title','The Big Lebowski'], ['year_released', '1998']]
 # favorite_movies = 'apple pie'
 
 raw_path = path_finder('The Shining', favorite_movies)
 puts raw_path.inspect
+puts raw_path.length
+puts favorite_movies.flatten.length
 puts "Enter this path to access your element: #{path_cleanup(raw_path)}"
